cb28121b2851a86745bc1cf656329f7340ea0b64
LEMEME <bms110403@gmail.com>
2019-11-14 17:31:30 +0000

    Update EventSender.cpp

--- src/Events/EventSender.cpp
+++ src/Events/EventSender.cpp
@@ -1,7 +1,21 @@
 #include "Events/EventSender.hpp"
 #include "Events/EventListener.hpp"
+#include <algorithm>
 
-void EventSender::addListener(EventListener* l)
+template <typename T>
+void EventSender<T>::addListener(T* l)
 {
     listeners.push_back(l);
+}
+
+template <typename T>
+void EventSender<T>::removeListener(T* l)
+{
+    listeners.erase(std::remove(listeners.begin(), listeners.end(), l), listeners.end());
+}
+
+template <typename T>
+void EventSender<T>::removeAll()
+{
+    listeners.clear();
 }
